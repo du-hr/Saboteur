e172b092903032a16445a711bafcba19d1906107
du-hr <duhaoran1998@gmail.com>
2020-04-12 11:26:16 -0400

    debug map card

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -14,11 +14,7 @@ public class StudentPlayer extends SaboteurPlayer {
     public static boolean goldTileRevealed = false;
     public static ArrayList<SaboteurMove> moves;
     public static ArrayList<SaboteurCard> cards;
-    public static int enemyID;
-    // -1 : DN
-    // 0: No
-    // 1: Yes
-    public static int[] isGold = {-1, -1, -1};
+    public static int[] goldCoord = new int[]{-1,-1};
 
     public StudentPlayer() {
 //        super("260784819"); // Cameron Cherif
@@ -38,6 +34,21 @@ public class StudentPlayer extends SaboteurPlayer {
         cards = boardState.getCurrentPlayerCards();
 //        System.out.println("Testing: Board " + Arrays.deepToString(boardState.getHiddenBoard()));
 //        System.out.println("Testing: Board " + boardState.getBoardForDisplay()[12][3].getIdx());
+        if (boardState.getBoardForDisplay()[12][3].getName().equals("Tile:nugget") || boardState.getBoardForDisplay()[12][5].getName().equals("Tile:nugget") || boardState.getBoardForDisplay()[12][7].getName().equals("Tile:nugget")){
+            goldTileRevealed = true;
+            if (boardState.getBoardForDisplay()[12][3].getName().equals("Tile:nugget")) {
+                goldCoord[0] = 12;
+                goldCoord[1] = 3;
+            }
+            if (boardState.getBoardForDisplay()[12][5].getName().equals("Tile:nugget")) {
+                goldCoord[0] = 12;
+                goldCoord[1] = 5;
+            }
+            if (boardState.getBoardForDisplay()[12][7].getName().equals("Tile:nugget")) {
+                goldCoord[0] = 12;
+                goldCoord[1] = 7;
+            }
+        }
         for (SaboteurCard card : cards)
             System.out.println("Testing: " + card.getName());
         for (SaboteurMove mov : moves)
@@ -49,73 +60,77 @@ public class StudentPlayer extends SaboteurPlayer {
     private int AIDecision(SaboteurBoardState boardState){
         SaboteurCard cardSelected = null;
 
-        // if in malus state, use the bonus card at once if possible
+        // if in malus state
         if (boardState.getNbMalus(boardState.getTurnPlayer()) > 0){
-            for (SaboteurCard card : this.cards) {
+            for (SaboteurCard card : cards) {
                 if (card instanceof SaboteurBonus)
                     cardSelected = card;
             }
-            for (SaboteurMove mov : this.moves){
-                if(mov.getCardPlayed().getName()== cardSelected.getName()) {
-                    return this.moves.indexOf(mov);
+            // use the bonus card at once if possible
+            if (cardSelected != null) {
+                for (SaboteurMove mov : moves) {
+                    if (mov.getCardPlayed().getName().equals(cardSelected.getName())) {
+                        return moves.indexOf(mov);
+                    }
                 }
             }
         }
 
-        // if the malus card is in hand, play it at once to reduce the chance of the random player messing up the path
-        for (SaboteurCard card : this.cards) {
-            if (card instanceof SaboteurMalus)
-                cardSelected = card;
-        }
-        for (SaboteurMove mov : this.moves){
-            if(mov.getCardPlayed().getName() == cardSelected.getName()){
-                return this.moves.indexOf(mov);
+        // if not in malus state
+        else{
+            // if the malus card is in hand, play it at once to reduce the chance of the random player messing up the path
+            for (SaboteurCard card : cards) {
+                if (card instanceof SaboteurMalus)
+                    cardSelected = card;
+            }
+            if (cardSelected != null) {
+                for (SaboteurMove mov : moves) {
+                    if (mov.getCardPlayed().getName().equals(cardSelected.getName())) {
+                        return moves.indexOf(mov);
+                    }
+                }
+            }
+
+            // if the gold card has not yet been revealed
+            if (!goldTileRevealed){
+                // check if the map card is hand
+                for (SaboteurCard card : cards) {
+                    if (card instanceof SaboteurMap)
+                        cardSelected = card;
+                }
+                if (cardSelected != null) {
+                    System.out.println("(12,3) :"+boardState.getBoardForDisplay()[12][3].getName());
+                    System.out.println("(12,5) :"+boardState.getBoardForDisplay()[12][5].getName());
+                    System.out.println("(12,7) :"+boardState.getBoardForDisplay()[12][7].getName());
+                    // 3, 5, 7
+                    if (boardState.getBoardForDisplay()[12][3].getName().equals("Tile:goalTile")) {
+                        System.out.println("goes here!!!!!!");
+                        for (SaboteurMove mov : moves) {
+                            // use the map card at the hidden object at once
+                            if (mov.getCardPlayed().getName().equals(cardSelected.getName()) && mov.getPosPlayed()[1] == 3)
+                                return moves.indexOf(mov);
+                        }
+                    } else if (boardState.getBoardForDisplay()[12][5].getName().equals("Tile:goalTile")) {
+                        System.out.println("goes here!!!!!!");
+                        for (SaboteurMove mov : moves) {
+                            // use the map card at the hidden object at once
+                            if (mov.getCardPlayed().getName().equals(cardSelected.getName()) && mov.getPosPlayed()[1] == 5)
+                                return moves.indexOf(mov);
+                        }
+                    } else if (boardState.getBoardForDisplay()[12][7].getName().equals("Tile:goalTile")) {
+                        System.out.println("goes here!!!!!!");
+                        for (SaboteurMove mov : moves) {
+                            // use the map card at the hidden object at once
+                            if (mov.getCardPlayed().getName().equals(cardSelected.getName()) && mov.getPosPlayed()[1] == 7)
+                                return moves.indexOf(mov);
+                        }
+                    }
+                }
             }
         }
-//
-//        // if the gold card has not yet been revealed, use the map card if possible
-//        if (!goldTileRevealed){
-//            // check if the map card is hand
-//            for (SaboteurCard card : cards) {
-//                if (card instanceof SaboteurMap)
-//                    cardSelected = card;
-//            }
-//            // 3, 5, 7
-//            for (SaboteurMove mov : moves) {
-//                if (mov.getCardPlayed().equals(cardSelected))
-//                    return moves.indexOf(mov);
-//            }
-////            if (boardState.getBoardForDisplay()[12][3].getName() == "goalTile"){
-////                for (SaboteurMove mov : moves) {
-////                    // use the map card at the hidden object at once
-////                    if (mov.getCardPlayed().equals(cardSelected) && mov.getPosPlayed()[1] == 3) {
-////                        goldTileRevealed = true;
-////                        isGold[0] = true;
-////                        return moves.indexOf(mov);
-////                    }
-////                }
-////            }
-////            else if (boardState.getBoardForDisplay()[12][5].getName() == "goalTile"){
-////                for (SaboteurMove mov : moves) {
-////                    // use the map card at the hidden object at once
-////                    if (mov.getCardPlayed().equals(cardSelected) && mov.getPosPlayed()[1] == 5) {
-////                        goldTileRevealed = true;
-////                        isGold[1] = true;
-////                        return moves.indexOf(mov);
-////                    }
-////                }
-////            }
-////            else if (boardState.getBoardForDisplay()[12][7].getName() == "goalTile") {
-////                for (SaboteurMove mov : moves) {
-////                    // use the map card at the hidden object at once
-////                    if (mov.getCardPlayed().equals(cardSelected) && mov.getPosPlayed()[1] == 7) {
-////                        goldTileRevealed = true;
-////                        isGold[2] = true;
-////                        return moves.indexOf(mov);
-////                    }
-////                }
-////            }
-//        }
+        return 0;
+    }
+
 //
 ////        // gold card is revealed
 ////        else {
@@ -133,9 +148,6 @@ public class StudentPlayer extends SaboteurPlayer {
 //
 //        // drop dead end cards in hand
 //        // greedy building the calculated path
-        return 0;
-    }
-
-
-
 }
+
+
