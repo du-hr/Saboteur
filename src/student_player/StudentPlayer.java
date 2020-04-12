986321161796e5caea63f58c8832a718ea2f3087
cameroncherif <cameron.cherif@outlook.com>
2020-04-11 20:36:20 -0400

    So this is where I am at so far, I will finish implementing the strategy after figuring out a) card indexes b) being able to manipulate the board as SaboteurTiles

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,33 +1,60 @@
 package student_player;
 
+import Saboteur.SaboteurMove;
+import Saboteur.cardClasses.SaboteurBonus;
+import Saboteur.cardClasses.SaboteurCard;
+import Saboteur.cardClasses.SaboteurDestroy;
+import Saboteur.cardClasses.SaboteurMalus;
 import boardgame.Move;
-
 import Saboteur.SaboteurPlayer;
 import Saboteur.SaboteurBoardState;
+import java.util.ArrayList;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends SaboteurPlayer {
-
 
     public StudentPlayer() {
         super("260784819 & 260776911");
     }
 
-//let's make one each and we'll just delete the other's constructor before submitting!
-/*
-    public StudentPlayer() {
-*        super("xxxxxxxxx");
-*    }
-*/
+    //add global variable for probability of discarding a card
+    // will increase with tile placements, and decrease with turn number
+
     /**
      * This is the primary method that you need to implement. The ``boardState``
      * object contains the current state of the game, which your agent must use to
      * make decisions.
      */
     public Move chooseMove(SaboteurBoardState boardState) {
-        // You probably will make separate functions in MyTools.
-        // For example, maybe you'll need to load some pre-processed best opening
-        // strategies...
+        int myTurn = boardState.getTurnPlayer();
+        boolean canDiscard = boardState.getTurnNumber() < 42;
+        boolean hasMalus = boardState.getNbMalus(myTurn) > 0;
+        SaboteurCard ownsMalus = null;
+        ArrayList<SaboteurCard> hand = boardState.getCurrentPlayerCards();
+        //create arraylists of int containing indx for diff cart types
+        //for our strat, let's consider destroys like dead end tiles. There are in total 12/56 cards that are discardable
+        System.out.println(boardState.getHiddenIntBoard());
+        for(SaboteurCard card : hand){
+            if(card instanceof SaboteurBonus && hasMalus)
+                return(new SaboteurMove(card, 0, 0, myTurn));
+            else if (card instanceof SaboteurMalus)
+                ownsMalus = card;
+            else if(card instanceof SaboteurDestroy)
+                continue;
+
+        }
+        //if enters the condition, then has malus and does not have a bonus
+        if(hasMalus){
+
+        }
+        if(canDiscard){
+
+        }else{
+            if(ownsMalus != null)
+                return(new SaboteurMove(card, 0, 0, myTurn));
+        }
+
+        System.out.println();
         MyTools.getSomething();
 
         // Is random the best you can do?
